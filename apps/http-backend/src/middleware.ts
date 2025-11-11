a1dc5a68642c275cb5b2a5d9d4d95513ab4a9e83
Rohs21 <rohans212004@gmail.com>
2025-11-11 17:24:31 +0530

    Created Middleware for Auth

---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import { JWT_SECRET } from "./config";
+import jwt from "jsonwebtoken";
+
+
+export function middleware(req: Request, res: Response, next: NextFunction){
+    const header = req.headers["authorization"] ?? "";
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if(decoded.userId){
+        req.userId = decoded.userId;
+        next();
+    }else{
+        res.status(403).json({
+            message: "Unauthorized";
+        })
+    }
+}
