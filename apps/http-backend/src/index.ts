240aef229bcaeb0dafc52d9299069f0f621b3386
Rohs21 <rohans212004@gmail.com>
2025-11-16 16:35:26 +0530

    Added Signup endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,23 +3,40 @@ import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from './middleware';
 import { CreateUserSchema} from "@repo/common/types";
+import { prismaClient } from "@repo/db/client"
+
 
 const app = express();
 
 const port = 3003;
 
-app.post("/signup",(req, res)=>{
+app.post("/signup", async(req, res)=>{
 
-    const data = CreateUserSchema.safeParse(req.body);
-    console.log(data);
-    if(!data.success){
+    const ParsedData = CreateUserSchema.safeParse(req.body);
+    console.log(ParsedData);
+    if(!ParsedData.success){
         return res.status(400).json({
             message: "Incorrect inputs",
         })
+        return;
     }
-    res.json({
-        userId: "123",
-    })
+    try{
+        await prismaClient.user.create({
+            data: {
+                email : ParsedData.data?.username,
+                password : ParsedData.data?.password,
+                name: ParsedData.data.name
+            }
+        })
+        res.json({
+            userId: "123",
+        })
+    }catch{
+        res.status(500).json({
+            message: "Something went wrong"
+        })
+    }
+    
 })
 
 app.post("/signin",(req, res)=>{
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -2,17 +2,25 @@ import { NextFunction, Request, Response } from "express";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import jwt from "jsonwebtoken";
 
+declare global {
+    namespace Express {
+        interface Request {
+            userId?: string;
+        }
+    }
+}
+
 export function middleware(req: Request, res: Response, next: NextFunction){
     
-    const token = req.headers["authorization"] ?? "";
-    const decoded = jwt.verify(token, JWT_SECRET);
+        const token = req.headers["authorization"] ?? "";
+        const decoded = jwt.verify(token, JWT_SECRET);
 
-    if(decoded.userId){
-        req.userId = decoded.userId;
-        next();
-    }else{
-        res.status(403).json({
-            message: "Unauthorized"
-        })
-    }
+        if((decoded as any).userId){
+                req.userId = (decoded as any).userId;
+                next();
+        }else{
+                res.status(403).json({
+                        message: "Unauthorized"
+                })
+        }
 }
