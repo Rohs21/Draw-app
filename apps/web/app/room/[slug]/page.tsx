9923d91d31de61df0cb202d599b566de88aa6d40
Rohs21 <rohans212004@gmail.com>
2025-11-21 23:52:56 +0530

    Error Solve of Slug

--- apps/web/app/room/[slug]/page.tsx
+++ apps/web/app/room/[slug]/page.tsx
@@ -5,8 +5,8 @@ import { ChatRoom } from "../../../components/ChatRoom";
 
 async function getRoomId(slug: string) {
     const response = await axios.get(`${BACKEND_URL}/room/${slug}`)
-    console.log(response.data);
-    return response.data.roomId;
+    console.log(response.data.roomId);
+    return response.data.room.id;
 }
 
 export default async function ChatRoom1({
